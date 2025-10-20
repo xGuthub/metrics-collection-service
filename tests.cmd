c3f10e1a012279835a59fe26bc107993117c6dc3
Roman Korolev <dev1@x12345.ru>
2025-10-20 19:25:49 +0300

    update tests script

--- tests.cmd
+++ tests.cmd
@@ -1,2 +1,3 @@
+go vet -vettool=statictest ./...
 metricstest.exe -test.v -test.run=^TestIteration1$ -binary-path=cmd/server/server
 metricstest.exe -test.v -test.run=^TestIteration2[AB]$ -source-path=. -agent-binary-path=cmd/agent/agent
